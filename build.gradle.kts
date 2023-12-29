c8f682c84689f88c4041342cd389ee64ebc5f063
Jeremy Karevich <cxomtdoh@yahoo.com>
2023-12-29 15:26:19 -0500

    Convert to gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+}
+
+group = "net.punchtree"
+version = "0.1.0-SNAPSHOT"
+description = "PunchTree-Util"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
