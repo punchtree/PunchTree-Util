a08c3217c2141054f70053021935526be6cd49a9
Jeremy Karevich <cxomtdoh@yahoo.com>
2024-07-30 13:51:46 -0400

    Don't force overriding both left and right click for interaction callback

--- src/main/java/net/punchtree/util/interaction/InteractionCallback.kt
+++ src/main/java/net/punchtree/util/interaction/InteractionCallback.kt
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player
 
 interface InteractionCallback {
 
-    fun onRightClick(interaction: Interaction, player: Player): Boolean
-    fun onLeftClick(interaction: Interaction, player: Player): Boolean
+    fun onRightClick(interaction: Interaction, player: Player): Boolean { return false }
+    fun onLeftClick(interaction: Interaction, player: Player): Boolean { return false }
 
 }
